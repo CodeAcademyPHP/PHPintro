14955d6521710f8b0ce9e893ee5281fff77d447e
Diana <diana@taskertools.com>
2022-06-14 10:26:27 +0300

    Second lesson excercises

---
+++ lesson2/excersices.php
@@ -0,0 +1,47 @@
+<?php
+declare(strict_types=1);
+
+/*
+1. Pašalinkite/užkomentuokite praeitos užduoties eilutes, kad jos netrukdytų toliau tęsti darbo.
+Išveskite į terminalą kintamojo $variableToDebug:
+- tik reikšmę
+- reikšmę kartu su duomens tipu
+*/
+$variableToDebug = 15;
+
+/*
+2. Sudėliokite masyvą ir jį atspausdinkite iš šių reikšmių:
+- Hello
+- Learning
+- Amazing
+- CodeAcademy
+- Php
+- Git
+*/
+
+/*
+3. Pakeiskite duotų kintamųjų reikšmes į:
+- int
+- bool
+*/
+
+//pakeisti i int:
+$variable1 = false;
+$variable2 = true;
+$variable3 = '2022 year';
+
+//pakeisti i bool:
+$variable4 = 150;
+$variable5 = [];
+$variable6 = 'Year - 2022';
+
+/*
+4. Atlikite šiuo loginius operatorius su duotais kintamaisiais taip, kad gautumėt true ir false
+- &&
+- ||
+*/
+
+$variable7 = 145;
+$variable8 = '145';
+$variable9 = '150';
+$variable10 = 150;
