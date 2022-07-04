33386bbffe11bf6417ea9ba0f86d55cb077258c7
Diana <diana@taskertools.com>
2022-07-04 21:53:20 +0300

    Adding another example

--- lesson9/DateTimeExample.php
+++ lesson9/DateTimeExample.php
@@ -11,3 +11,7 @@ $timeZone = 'America/Matamoros';
 $modifiedDate = $date->setTimezone(new DateTimeZone($timeZone));
 
 $modifiedDateWithFormat = $modifiedDate->format('Y.m.d H:i');
+
+//sukurimas is formato
+$date3 = '2000 March 2nd 15:30:00';
+$date = DateTime::createFromFormat('Y F jS H:i:s', $date3);
